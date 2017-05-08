3cc85b29a60e47835d7103882ac09dee2c025aff
pablo <pabloejacome@gmail.com>
2017-05-08 13:54:53 -0700

    set up test function. now to write tests

--- package_installer.js
+++ package_installer.js
@@ -3,14 +3,47 @@ var input = ['KittenService: CamelCaser', 'CamelCaser: '];
 /*
     @packages - an array of strings containing the packages and their dependencies
                 ex: ['KittenService: CamelCaser', 'CamelCaser: ']
+    returns String - the string will be the correct order in which the packages
+                   - need to be installed
 */
 
-module.exports.PACKAGE_ORDER = function(packages) {
+var t = module.exports.PACKAGE_ORDER = function(packages) {
     // 1. verify 'packages' is of type array
     // 2. check if 'packages' is not equal to length 0
     // 3. check if 'packages' is equal to size 1
     // 4. else continue
     packages.forEach(function(val, i) {
-        console.log(i+') ', val);
+        // console.log(i+') ', val);
+        var package = val.split(':')[0];
+        var dependency = val.split(':')[1];
+        // console.log(package + ' -> ' + dependency);
     });
+    // -1 is a placeholder while tests are set up
+    return -1;
+}
+
+/*
+    Runs all tests.
+*/
+
+module.exports.RUN_TESTS = function() {
+    console.log(testPackageInstaller([], ''));
+    console.log(testPackageInstaller(['A: '], 'A'));
+    console.log(testPackageInstaller([], 'CamelCaser, KittenService: CamelCaser'));
+}
+
+function testPackageInstaller(input, expected) {
+    var result = t(input);
+    // are they even the same length?
+    if(result.length !== expected.length) {
+        return 'FAIL';
+    }
+
+    for(var i = 0; i < expected.length; ++i) {
+        if(!result[i].match(expected[i])) {
+            return 'FAIL';
+        }
+    }
+
+    return 'PASS';
 }
