1de321a80bd5ba4aa74ba02afe789377a1302ab8
pablo <pabloejacome@gmail.com>
2017-05-08 16:18:44 -0700

    passing first couplpe basic tests

--- package_installer.js
+++ package_installer.js
@@ -1,5 +1,3 @@
-var input = ['KittenService: CamelCaser', 'CamelCaser: '];
-
 /*
     @packages - an array of strings containing the packages and their dependencies
                 ex: ['KittenService: CamelCaser', 'CamelCaser: ']
@@ -12,14 +10,27 @@ var orderPackages = module.exports.PACKAGE_ORDER = function(packages) {
     // 2. check if 'packages' is not equal to length 0
     // 3. check if 'packages' is equal to size 1
     // 4. else continue
+
+    // section 1.
+
+    // section 2.
+    if(packages.length === 0)
+        return '';
+    
+    // section 3.
+    if(packages.length === 1)
+        return packages[0].split(': ')[0];
+
+    // section 4.
     packages.forEach(function(val, i) {
         // console.log(i+') ', val);
-        var package = val.split(':')[0];
-        var dependency = val.split(':')[1];
+        var package = val.split(': ')[0];
+        var dependency = val.split(': ')[1];
         // console.log(package + ' -> ' + dependency);
     });
+
     // -1 is a placeholder while tests are set up
-    return -1;
+    return '-1';
 }
 
 /*
@@ -29,23 +40,25 @@ var orderPackages = module.exports.PACKAGE_ORDER = function(packages) {
 module.exports.RUN_TESTS = function() {
     Print(TestPackageInstaller([], ''));
     Print(TestPackageInstaller(['A: '], 'A'));
-    Print(TestPackageInstaller([], 'CamelCaser, KittenService'));
+    Print(TestPackageInstaller(['KittenService: CamelCaser', 'CamelCaser: '], 'CamelCaser, KittenService'));
     Print(TestPackageInstaller([], 'A, C, B'));
-    Print(TestPackageInstaller([], '')); 
+    Print(TestPackageInstaller([], 'A, B, C, D, E, F')); 
     // 5
-    Print(TestPackageInstaller([], ''));
-    Print(TestPackageInstaller([], ''));
-    Print(TestPackageInstaller([], ''));
-    Print(TestPackageInstaller([], ''));
-    Print(TestPackageInstaller([], '')); 
+    Print(TestPackageInstaller([], 'A, B, C, D, F, E'));
+    Print(TestPackageInstaller([], 'F, E, D, C, B, A'));
+    Print(TestPackageInstaller(['A: B', 'B: A'], 'A, B, A') + ': Contains a Cycle');
+    Print(TestPackageInstaller(['A: ', 'B: A'], 'A, B'));
+    Print(TestPackageInstaller(['KittenService: ', 'Leetmeme: Cyberportal', 'Cyberportal: Ice', 'CamelCaser: KittenService', 'Fraudstream: Leetmeme', 'Ice: '], 'KittenService, Ice, Cyberportal, Leetmeme, CamelCaser, Fraudstream')); 
     // 10
-    Print(TestPackageInstaller([], ''));
-    Print(TestPackageInstaller([], ''));
-    Print(TestPackageInstaller([], ''));
-    Print(TestPackageInstaller([], ''));
-    Print(TestPackageInstaller([], ''));
-    // 15
 }
+
+/*
+    Tests the package installer
+    @input - an array of packages and their dependencies
+    @expected - the expected String after running the package installer
+    returns String - 'PASS' if the expected matches the result
+                   - 'FAIL' otherwise
+*/
 
 function TestPackageInstaller(input, expected) {
     var result = orderPackages(input);
@@ -61,6 +74,6 @@ function TestPackageInstaller(input, expected) {
     Simpler console.log function to save a bit of time.
 */
 
-function Print(string) {
-    console.log(string);
+function Print(result) {
+    console.log(result);
 }
